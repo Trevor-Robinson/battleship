80f52749ad1d710b391d88700a624a4c75bd8713
Trevor Robinson <trevor.robi1254@gmail.com>
2020-12-08 20:04:34 -0700

    Create seperate_coords helper method

--- test/board_test.rb
+++ test/board_test.rb
@@ -23,4 +23,38 @@ class Boardtest < Minitest::Test
 
     assert_equal false, board.valid_coord?("E4")
   end
+
+  def test_valid_placement_length?
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+
+  end
+
+  def test_valid_placement_consecutive?
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+  end
+
+  def test_seperate_coords_num
+    board = Board.new
+    coords = ["A1", "A2", "A4"]
+    coord_num, coord_letter = board.seperate_coords(coords)
+    # require 'pry'; binding.pry
+
+    assert_equal [1, 2, 4], coord_num
+  end
+
+  def test_seperate_coords_letter
+    board = Board.new
+    coords = ["A1", "A2", "A4"]
+    coord_num, coord_letter = board.seperate_coords(coords)
+    # require 'pry'; binding.pry
+
+    assert_equal [65, 65, 65], coord_letter
+  end
 end
