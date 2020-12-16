947add7c8f5ae6f694e983a3461851cfa8081353
Harrison Blake <harrison.r.blake@gmail.com>
2020-12-16 16:22:35 -0700

    Complete ai fire feature

--- test/game_test.rb
+++ test/game_test.rb
@@ -27,7 +27,6 @@ class Gametest < Minitest::Test
   end
 
   def test_player_can_place_ship
-    skip
     game = Game.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -39,7 +38,6 @@ class Gametest < Minitest::Test
   end
 
   def test_player_can_fire
-    skip
     game = Game.new
     game.coord_to_fire_on
     game.coord_to_fire_on
@@ -85,22 +83,11 @@ class Gametest < Minitest::Test
   end
 
   def test_end_game
-    skip
     game = Game.new
 
-    p_cruiser, p_submarine = game.create_ships_default
-    h_cruiser, h_submarine = game.create_ships_default
+    game.create_ships_default
 
-    p_cruiser.hit
-    p_cruiser.hit
-    p_cruiser.hit
-    p_submarine.hit
-    p_submarine.hit
-
-    p_cruiser.sunk?
-    p_submarine.sunk?
-
-    assert_equal true, game.end_game(p_cruiser, p_submarine, h_cruiser, h_submarine)
+    assert_equal false, game.computer_win_condition
   end
 
   def test_get_coords
@@ -113,10 +100,33 @@ class Gametest < Minitest::Test
     assert_equal ["B2", "A2"], game.get_coords_up(submarine, "B2")
   end
 
-  def test_custom_ships
+  def test_determine_hit_miss_sunk
+    game = Game.new
+    ship = game.create_ship_custom("Ship", 2)
+
+    game.player_place_ship(ship)
+
+    game.player_board.cells["A1"].fire_upon
+    game.player_board.cells["A4"].fire_upon
+
+    assert_equal "M", game.determine_hit_miss_sunk("A4", game.player_board)
+  end
+
+  def test_get_coord
     game = Game.new
 
-    game.play_game
+    assert_equal "A3", game.get_right("A2")
+    assert_equal "A1", game.get_left("A2")
+    assert_equal "B1", game.get_up("A1")
+    assert_equal "B1", game.get_down("C1")
+  end
 
+
+  def test_surrounding_coords
+    game = Game.new
+    ship = game.create_ship_custom("Ship", 2)
+    game.player_board.cells["C3"].fire_upon
+
+    assert_equal ["A3", "B4", "B2"], game.surrounding_coords("B3")
   end
 end
